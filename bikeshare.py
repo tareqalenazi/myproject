c12860c1925472e89de2a60cba595ed91d7e8339
tareqalenazi <75435200+tareqalenazi@users.noreply.github.com>
2020-12-25 01:17:48 +0300

    Update bikeshare.py
    modify

--- bikeshare.py
+++ bikeshare.py
@@ -20,6 +20,7 @@ def get_filters():
     """
 
     print('\nHello! Let\'s explore some US bikeshare data!')
+    
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
     while True:
@@ -29,6 +30,7 @@ def get_filters():
             continue
         else:
             break
+            
     # TO DO: get user input for month (all, january, february, ... , june)
 
     while True:
@@ -38,6 +40,7 @@ def get_filters():
             continue
         else:
             break
+            
     # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
 
     while True:
