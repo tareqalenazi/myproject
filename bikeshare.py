c64096f57dbb79aaf569ef015c50488386646577
tareqalenazi <75435200+tareqalenazi@users.noreply.github.com>
2020-12-25 20:43:27 +0300

    improve code

--- bikeshare.py
+++ bikeshare.py
@@ -38,6 +38,7 @@ def get_filters():
             continue
         else:
             break
+            
     # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
 
     while True:
